10f41f41cbdf2e4a2e946ca9ca1d9f37ac96dcbf
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-07-14 17:26:46 -0400

    Ran Prettier 🏃🏼‍♀️✨

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Button from '@mui/material/Button'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
 
 import TextBlock from "./typeComponents/TextBlock";
 import MainHeading from "./typeComponents/MainHeading";
@@ -11,31 +11,37 @@ import SubHeading from "./typeComponents/SubHeading";
 const auTheme = createTheme({
   palette: {
     twilight: {
-      main: '#656284',
-      light: '#AFADC2',
-      dark: '#3A3659',
-      contrastText: '#D7D6E1',
+      main: "#656284",
+      light: "#AFADC2",
+      dark: "#3A3659",
+      contrastText: "#D7D6E1",
     },
     blackberryJam: {
-      main: '#B689A2',
-      light: '#E2D0DA',
-      dark: '#784A63',
-      contrastText: '#F7F2F5',
-    }
-  }
-})
+      main: "#B689A2",
+      light: "#E2D0DA",
+      dark: "#784A63",
+      contrastText: "#F7F2F5",
+    },
+  },
+});
 
 export default function App() {
   return (
     <ThemeProvider theme={auTheme}>
-      <TextBlock sx={{bgcolor: 'twilight.light'}}>
+      <TextBlock sx={{ bgcolor: "twilight.light" }}>
         <MainHeading> Aurora's Test Page?</MainHeading>
         <SubHeading> u get a bunch of decomp facts app.</SubHeading>
 
-        <Paragraph>Did you know that after you die, a lot of your tissues liquefy. This is not due to decomposition, but due to an built-in mechanism in your cells?</Paragraph>
+        <Paragraph>
+          Did you know that after you die, a lot of your tissues liquefy. This
+          is not due to decomposition, but due to an built-in mechanism in your
+          cells?
+        </Paragraph>
 
-        <Button variant="contained" color="blackberryJam">Guau, great fact!</Button>
-      </TextBlock> 
-      </ThemeProvider>
+        <Button variant="contained" color="blackberryJam">
+          Guau, great fact!
+        </Button>
+      </TextBlock>
+    </ThemeProvider>
   );
 }
