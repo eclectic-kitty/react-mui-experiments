ccd1b192bb7daa2af2a60547ccc868cbbbf1f367
Aurora Becerra Granados <78286941+eclectic-kitty@users.noreply.github.com>
2025-08-04 09:34:06 -0400

    1st Grid experiments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import * as React from "react";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import Button from "@mui/material/Button";
+import { Grid2 as Grid, Paper, Typography, Button } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import TextBlock from "./typeComponents/TextBlock";
 import MainHeading from "./typeComponents/MainHeading";
 import Paragraph from "./typeComponents/Paragraph";
 import SubHeading from "./typeComponents/SubHeading";
 
+import DecompFacts from "./DecompFacts";
+
 const auTheme = createTheme({
   palette: {
+    background: {
+      default: "#EBEBF0",
+      paper: "#EEEBFC",
+    },
+    text: {
+      primary: "#383649",
+    },
     twilight: {
       main: "#656284",
       light: "#AFADC2",
@@ -25,23 +35,46 @@ const auTheme = createTheme({
   },
 });
 
-export default function App() {
+const App = () => {
   return (
     <ThemeProvider theme={auTheme}>
-      <TextBlock sx={{ bgcolor: "twilight.light" }}>
-        <MainHeading> Aurora's Test Page?</MainHeading>
-        <SubHeading> u get a bunch of decomp facts app.</SubHeading>
+      <CssBaseline />
+      <Grid container columns={12} spacing={3} marginTop={8} marginX={25}>
+        {/* Header */}
+        <Grid size={12}>
+          <Paper elevation={8} sx={{ padding: 2 }}>
+            <Typography variant="h2" component="h1" align="center">
+              Aurora's Test Page?
+            </Typography>
+          </Paper>
+        </Grid>
 
-        <Paragraph>
-          Did you know that after you die, a lot of your tissues liquefy. This
-          is not due to decomposition, but due to an built-in mechanism in your
-          cells?
-        </Paragraph>
+        {/* Content */}
+        <DecompFacts></DecompFacts>
+        <Grid size={4}>
+          <Paper elevation={4}>
+            <Typography variant="h4" component="h3">
+              some info about ur gorgeous and benevolent teacher
+            </Typography>
+          </Paper>
+        </Grid>
 
-        <Button variant="contained" color="blackberryJam">
-          Guau, great fact!
-        </Button>
-      </TextBlock>
+        {/* <TextBlock sx={{ bgcolor: "twilight.light" }}>
+          <SubHeading> u get a bunch of decomp facts app.</SubHeading>
+
+          <Paragraph>
+            Did you know that after you die, a lot of your tissues liquefy. This
+            is not due to decomposition, but due to an built-in mechanism in
+            your cells?
+          </Paragraph>
+
+          <Button variant="contained" color="blackberryJam">
+            Guau, great fact!
+          </Button>
+        </TextBlock> */}
+      </Grid>
     </ThemeProvider>
   );
-}
+};
+
+export default App;
